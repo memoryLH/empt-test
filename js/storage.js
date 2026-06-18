90d475439c8acc5df2212c6a26714ffe64f8de02
memoryLH <1925792024@qq.com>
2026-06-18 18:16:33 +0800

    ·

--- js/storage.js
+++ js/storage.js
@@ -1,3 +1,18 @@
 (function () {
   'use strict';
+
+  var HIGH_SCORE_KEY = 'snakeHighScore';
+
+  function getHighScore() {
+    var storedScore = window.localStorage.getItem(HIGH_SCORE_KEY);
+
+    return Number.parseInt(storedScore, 10) || 0;
+  }
+
+  function setHighScore(score) {
+    window.localStorage.setItem(HIGH_SCORE_KEY, String(score));
+  }
+
+  window.getHighScore = getHighScore;
+  window.setHighScore = setHighScore;
 }());
